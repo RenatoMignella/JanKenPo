30428f38d98a3a8279c4238db4d2431dc3efa73d
Renato Mignella <renatoferigato@gmail.com>
2022-06-09 17:21:43 +0100

    function difining chices

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#random imported to generate the random value 
+import random
+
+def game():
+    """
+    Input for the user chose and machine will ramdom chose as well 
+    """
+    player = input("Please chose betwin rock , paper and scissors ")
+    machine = random.choice(['rock', 'paper', 'scissors'])
+    
