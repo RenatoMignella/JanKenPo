8fef9669253c03ac521aff5a22a5ceabbaeb0421
Renato Mignella <renatoferigato@gmail.com>
2022-06-09 17:54:08 +0100

    pyfiglet add and welcome message

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 #random imported to generate the random value 
 import random
+#pyfiglet imported to generate nice interface 
+import pyfiglet
+
+# Print Welcome message
+banner = pyfiglet.figlet_format("JunKenPo")
+print(banner)
+
+print("Welcome to JunKenPo\n")
+print("Please chose betwin rock paper and scissors and challenge the Computer !!\n")
 
 def game():
     """
@@ -10,10 +19,28 @@ def game():
     
     """
     Rules 
-    two equals Draw 
+    two equals draw 
     rock beats scissors , scissors beats paper , 
     and paper beats rock 
     """
     if player == machine:
-        return "draw"
+        return "Result is: draw"
+    
+    if win_game(player, machine):
+        return 'Result is: You win congatulations !!'
+    
+    return 'Result is: You lost sorry '
+
+
+
+def win_game(player1, computer1):
+    """
+    It will return true if the player win 
+    rock beats scissors , scissors beats paper , 
+    and paper beats rock 
+    """   
+    if (player1 == 'rock' and computer1 == 'scissors') or \
+        (player1 == 'scissors' and computer1 == 'paper')\
+            or (player1 == 'paper' and computer1 == 'rock'):
+                return True
     
