429ee4e096fafceb8f7daf314a0df7536ffca91c
Renato Mignella <renatoferigato@gmail.com>
2022-06-16 18:55:51 +0100

    indentation fix Readme info add

--- README.md
+++ README.md
@@ -8,4 +8,92 @@
 
 ![Responsice Mockup](/assets/images/AmIResponsiveScrenshot.JPG)
 
-## Features 
+## How to play 
+
+JanKenPo is a classic Rock Paper and Scissors game. 
+######
+The user can play against the computer by inputting the values on the terminal. 
+######
+The game will tell the player immediatley when win , lose or draw.
+
+##Fratures
+####
+####Existing Features
+<img alt="Welcome" src="./assets/images/WelcomeScreenshot.jpg" width="80%">
+
+- Randon computer selection 
+  - Once the player choose computer will make a ramdom selection 
+   - The player will not be able to see that
+####
+- Example when the player win  
+
+<img alt="Win" src="./assets/images/WinScreenshot.jpg" width="80%">
+
+###
+
+- Example when the player lose  
+
+<img alt="Lose" src="./assets/images/LoseScreenshot.jpg" width="80%">
+
+###
+
+- Example when the player Draw 
+  
+
+<img alt="Draw" src="./assets/images/DrawScreenshot.jpg" width="80%">
+
+###
+- Play again 
+  - When the game is finished the user will be prompeted to press 'r' to play again or 'q' to exit the game.
+###
+  <img alt="Draw" src="./assets/images/PlayAgainSceenshot.jpg" width="90%">
+
+  ####
+  - Exit game 
+    - once the player press 'q' to exit it will be showed a banner and the game will finish.
+    ####
+      <img alt="Draw" src="./assets/images/quitScreenshot.jpg" width="90%">
+
+      ####
+- Input validation and error checking 
+  - 1
+<img alt="validation" src="./assets/images/.jpg" width="90%">
+
+####
+
+###Testing
+
+I have tested the project doing the following
+
+- passed on [PEP8](http://pep8online.com/).
+- Tried invalid inputs 
+- Tested on Heroku and the Terminal.
+
+  #### Bugs
+
+  Solved Bugs
+ - Validation problem
+ 
+
+  #### Remain Bugs
+
+- No remain Bugs 
+
+ #### Validator Testing 
+ - [PEP8](http://pep8online.com/)
+   - Initially found few errors that where fixed only 2 Warnings left
+ #### Deployment 
+- When you create the app, you will need to add two buildpacks from the Settings tab. The ordering is as follows:
+
+   - heroku/python
+    - heroku/nodejs
+    - You must then create a Config Var called - PORT. Set this to 8000
+
+    
+  -  Connect your GitHub repository and deploy as normal.
+
+  #### Credits
+
+   - [W3Schools](https://www.w3schools.com/python/python_datetime.asp)
+   - [Code Institute](https://www.CodeInstitute.net)
+   - [Youtube](https://www.youtube.com/)
--- run.py
+++ run.py
@@ -1,74 +1,86 @@
-#random imported to generate the random value
+# random imported to generate the random value
 import random
-#pyfiglet imported to generate nice interface
+
+# pyfiglet imported to generate nice interface
 import pyfiglet
 
+
 def welcome():
-    """Print Welcome message"""
-    
-    banner = pyfiglet.figlet_format("JanKenPo.")
+    """
+    Print Welcome message
+    """
+    banner = pyfiglet.figlet_format("JanKenPo")
     print(banner)
 
     print("Welcome to JanKenPo\n")
-    print("Please choose betweenn rock paper and scissors and challenge the Computer !!\n")
-    print("Please type only lower case letters Example: paper , rock , scissors\n")
+    print(
+     "Please choose betweenn rock paper and scissors and "
+     "challenge the Computer !!\n"
+    )
+    print("Please type only lower case letters Example: "
+          "paper , rock , scissors\n")
+
 
 def game():
     """
-    Input for the user chose and machine will ramdom chose as well 
-    and Rules: two equals draw rock beats scissors , scissors beats paper , 
+    Input for the user chose and machine will ramdom chose as well
+    and Rules: two equals draw rock beats scissors , scissors beats paper,
     and paper beats rock
     """
     player = input("The computer has chosen, It's your turn:\n")
-    machine = random.choice(['rock', 'paper', 'scissors'])
-                 
+    machine = random.choice(["rock", "paper", "scissors"])
+
     if player == machine:
-        return 'Result is: Draw !'
-              
+        return "Result is: Draw !"
+
     if win_game(player, machine):
-        return 'Result is: You win congatulations !!'
-    
-    return 'Result is: You lost sorry..'
-    
+        return "Result is: You win congatulations !!"
+
+    return "Result is: You lost sorry.."
+
     # while True:
-        
+
     #  if validade_input(player):
     #     print('deu certo')
     #     break
 
+
 def win_game(player1, computer1):
     """
-    It will return true if the player win 
-    rock beats scissors , scissors beats paper , 
-    and paper beats rock 
-    """   
-    if (player1 == 'rock' and computer1 == 'scissors') or \
-        (player1 == 'scissors' and computer1 == 'paper')\
-            or (player1 == 'paper' and computer1 == 'rock'):
-                return True
-               
+    It will return true if the player win
+    rock beats scissors , scissors beats paper ,
+    and paper beats rock
+    """
+    if (
+        (player1 == "rock" and computer1 == "scissors")
+        or (player1 == "scissors" and computer1 == "paper")
+        or (player1 == "paper" and computer1 == "rock")
+    ):
+        return True
+
+
 def play_again():
     """
-    function to define play again or exit 
+    function to define play again or exit
     """
-    
+
     while True:
-      continu = input("To play again press'r', to finish press 'q'\n")
-      if continu == 'r':
-        print(game())
-      else:
-          banner2 = pyfiglet.figlet_format("Thank you for playng !")
-          print(banner2)
-          break
-          
-            
+        continu = input("To play again press'r', to finish press 'q'\n")
+        if continu == "r":
+            print(game())
+        else:
+            banner2 = pyfiglet.figlet_format("Thank you for playng !")
+            print(banner2)
+            break
+
+
 # def validade_input(player):
 #     """
-#     this function will validade all awnsers that the use input 
-    
+#     this function will validade all awnsers that the use input
+
 #     """
 #     try:
-        
+
 #         if player != 'rock' and player != 'paper' and player !='scissors':
 #            raise ValueError(
 #                 f"you must to type paper , rock or scissors "
@@ -76,21 +88,20 @@ def play_again():
 #     except ValueError as e:
 #         print(f"invalid data: {e}, please try again .\n")
 #         return False
-        
-#     return True  
-    
-    
-   
+
+#     return True
+
+
 def main():
     """
     main function that will call all other ones
     Ref love sandwichs project
-    """                 
-welcome()          
+    """
+
+
+welcome()
 print(game())
 play_again()
-      
 
 
 print(main())
-   
